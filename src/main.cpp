015343cbbc84844acdd6fc83b6c2e13fc66d9e95
Daniil Chumachenko <chimi@pluto4.pluto4>
2026-02-26 13:49:05 +0300

    add input from file

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,25 @@
 #include "lexer.hpp"
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main(int argc, char* argv[]) {
-  std::string input = "var x = 123b; print x + 5;";
-  std::cout << "INPUT:\n" << input << std::endl;
+  if (argc < 2) {
+    std::cerr << "No file specified!\n";
+    return -1;
+  }
+  std::ifstream fin(argv[1]);
+  if (!fin.is_open()) {
+    std::cerr << "No such file or directory " << argv[1] << std::endl;
+    return -2;
+  }
+  std::string input;
+  for (std::string l; getline(fin, l);)
+    input += l;
+  fin.close();
   Lexer lexer(input);
   std::vector<Token> tokens = lexer.tokenize();
-  std::cout << "OUTPUT:\n";
   for (Token& token : tokens) {
     std::cout << token.to_string() << std::endl;
   }
