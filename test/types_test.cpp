3922981d13fd6fb0b3a46c5113a03b2d06c8b431
chimi <deadxraver@yandex.ru>
2026-04-29 12:49:15 +0300

    add test for type_setter

--- test/types_test.cpp
+++ test/types_test.cpp
@@ -1,6 +1,42 @@
 #include "statement.hpp"
+#include "type_setter.hpp"
 
 #include <iostream>
+#include <vector>
+#include <stdexcept>
+
+void type_setter_test(void) {
+  std::vector<std::unique_ptr<Statement>> ast;
+  ast.push_back(
+    std::make_unique<VarStatement>("x")
+  );
+  VarStatement* x =
+    static_cast<VarStatement*>(ast[0].get());
+  ast.push_back(
+    std::make_unique<VarStatement>(
+      "y",
+      std::make_unique<StringExpression>("I am string")
+    )
+  );
+  VarStatement* y =
+    static_cast<VarStatement*>(ast[1].get());
+  ast.push_back(
+    std::make_unique<ExpressionStatement>(
+      std::make_unique<AssignExpression>(
+        "x",
+        std::make_unique<NumberExpression>(42)
+      )
+    )
+  );
+  TypeSetter ts(ast);
+  ts.figure_out();
+  if (x->data_type() != dtNumber) {
+    throw std::runtime_error("`x` expected to be number");
+  }
+  if (y->data_type() != dtString) {
+    throw std::runtime_error("`y` expected to be string");
+  }
+}
 
 int main(void) {
   VarStatement st("x");
@@ -18,6 +54,8 @@ int main(void) {
     std::cerr << "st.data_type didn't change after assignment\n";
     return -1;
   }
+
+  type_setter_test();
 
   return 0;
 }
